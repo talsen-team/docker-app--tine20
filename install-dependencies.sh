addd62eaa87a9a426eb1a7866cca3a5a40629e2f
David Lechner <david.lechner@talsen.team>
2019-02-08 19:15:27 +0100

    add dependencies installation script

---
+++ install-dependencies.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Add VS Code repository to apt-get ..."
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+echo "Add VS Code repository to apt-get ... done"
+
+echo "Install VS Code ..."
+apt-get install -y --no-install-recommends \
+                apt-transport-https
+apt-get update
+apt-get install -y --no-install-recommends \
+                code
+echo "Install VS Code ... done"
+
+echo "Add docker repository to apt-get ..."
+apt-get update
+apt-get install -y --no-install-recommends \
+                apt-transport-https        \
+                ca-certificates            \
+                curl                       \
+                gnupg-agent                \
+                software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository                                            \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs)                                         \
+   stable"
+echo "Add docker repository to apt-get ... done"
+
+echo "Install docker ..."
+apt-get update
+apt-get install -y --no-install-recommends \
+                docker-ce                  \
+                docker-ce-cli              \
+                containerd.io
+echo "Install docker ... done"
+
+echo "Install docker-compose ..."
+apt-get update
+apt-get install -y --no-install-recommends \
+                docker-compose
+echo "Install docker-compose ... done"
+
+echo "Install ssh-askpass-fullscreen ..."
+apt-get install -y --no-install-recommends \
+                ssh-askpass-fullscreen
+echo "Install ssh-askpass-fullscreen ... done"
