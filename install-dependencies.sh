b4c410caaae8c63babf4c33816a81930b83a887c
David Lechner <david.lechner@talsen.team>
2019-02-08 19:50:58 +0100

    fix docker compose installation

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -46,9 +46,9 @@ apt-get install -y --no-install-recommends \
 echo "Install docker ... done"
 
 echo "Install docker-compose ..."
-apt-get update
-apt-get install -y --no-install-recommends \
-                docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 echo "Install docker-compose ... done"
 
 echo "Install ssh-askpass-fullscreen ..."
